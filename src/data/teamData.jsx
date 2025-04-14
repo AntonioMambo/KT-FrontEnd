a609cf9eb8bff1b7a35b10a750b05bfdc3fdb837
AntonioMambo <tonymambo89@gmail.com>
2025-04-14 17:25:23 +0200

    Outro Commit 2.1

--- src/data/teamData.jsx
+++ src/data/teamData.jsx
@@ -1,10 +1,3 @@
-import user1 from "/images/user1.jpg";
-import user2 from "/images/user2.jpg";
-import user3 from "/images/user3.jpg";
-import user4 from "/images/user4.jpg";
-import user5 from "/images/user5.jpg";
-import user6 from "/images/user5.jpg";
-
 const teamData = [
   {
     nome: "Lionel Júnior",
@@ -13,7 +6,7 @@ const teamData = [
       "Defensor dos Direitos Humanos, com foco na promoção e proteção dos direitos das pessoas LGBTQIA+. A sua experiência de trabalho abrange intervenções em comunidades urbanas e rurais, bem como a gestão e mobilização de movimentos juvenis. Ao longo da sua trajectória de activismo, Lionel integrou-se em importantes iniciativas, como o projecto +PorNós, cujo objectivo é ampliar o acesso à informação e aos serviços de saúde para populações-chave. Destaca-se também pela sua actuação no projecto Empreender Com Orgulho, que visa influenciar positivamente a redução da desigualdade socioeconómica enfrentada por pessoas LGBTQIA+ em Moçambique. Profissional multifacetado, Lionel tem-se destacado na organização de eventos que promovem a visibilidade positiva da comunidade LGBTQIA+ e na criação de espaços seguros de partilha, empoderamento e fortalecimento de redes comunitárias.",
     frase_motivacional:
       "Blinde-se com o conhecimento das leis. Antes de lutares pelos teus direitos, conhece os teus direitos.",
-    imagem: user1,
+    imagem: "/images/user1.jpg",
   },
   {
     nome: "Tarik Azizi",
@@ -22,7 +15,7 @@ const teamData = [
       "Licenciado em Linguística pela Universidade Eduardo Mondlane, treinado em gestão de projectos sociais, gestão administrativa e financeira para organizações. A sua experiência de trabalho inclui o desenvolvimento de trabalho para Associação Kucthintcha, Free to be Me, Orange Corners e Oficina de sucesso.",
     frase_motivacional:
       "Se não fizeres hoje, amanhã, o trabalho será a dobrar.",
-    imagem: user2,
+    imagem: "/images/user2.jpg",
   },
   {
     nome: "Joaquim Macaringue",
@@ -31,7 +24,7 @@ const teamData = [
       "É licenciado em Educação e Assistência Social pela Universidade Pedagógica, tem uma ampla experiência no desenvolvimento e implementação de projetos ligados à promoção dos direitos humanos e econômicos de pessoas LGBTQIA+ em Moçambique. É activista e defensor dos direitos humanos com foco nas pessoas LGBTQIA+, promove o respeito pela diversidade e inclusão de jovens em situação de vulnerabilidade e exclusão, usando da advocacia e do artivismo para defender os direitos humanos. Desde 2022, é Coordenador da Associação Kutchindja, que tem como principal objetivo advocar pelos direitos humanos e influenciar no processo de revisão de leis e políticas públicas a favor de pessoas LGBTQIA+, além de promover iniciativas de Inclusão Social da Juventude e das Diversidades.",
     frase_motivacional:
       "Cada passo em direção à inclusão transforma vidas. Pessoas LGBTQIA+ são mais que siglas; merecem respeito e inclusão em todas as esferas da sociedade. Juntos, erguemo-nos em defesa da diversidade e pela mudança que precisamos sentir e ver.",
-    imagem: user3,
+    imagem: "/images/user3.jpg",
   },
   {
     nome: "Abdul Amid Bacar",
@@ -39,7 +32,7 @@ const teamData = [
     biografia:
       "Abdul Amid Bacar tem mais de 3 anos de experiência na área de Monitoria, Avaliação e Responsabilização, trabalhando em diversos projectos que tenham como beneficiários pessoas LGBTQIA+. Ele participou de série de treinamento de Monitoria e Avaliação, entre eles o treinamento da HIVOS e do Passos. Abdul Amid Bacar é oficial de programas, comprometido em garantir a qualidade e eficiência das actividades realizadas a nível da associação Kutchindja.",
     frase_motivacional: "A persistência realiza o impossível.",
-    imagem: user4,
+    imagem: "/images/user4.jpg",
   },
   {
     nome: "Marcos Chambe",
@@ -48,7 +41,7 @@ const teamData = [
       "Marcos Chambe é estudante de Psicologia das Organizações pela Universidade Eduardo Mondlane. Ele se dedica a projetos voltados para a promoção dos direitos humanos e econômicos das pessoas LGBTQIA+ em Moçambique. Como ativista e defensor dos direitos humanos, Marcos foca na comunicação para a mudança de comportamento. Em 2022, iniciou sua carreira profissional como ciberativista, descobrindo sua paixão pela ajuda voluntária e pelo engajamento social, sempre buscando promover a mudança de comportamento através da comunicação.",
     frase_motivacional:
       "Cada desafio é uma oportunidade de autodescoberta e realização, você pode muito mais do que imagina.",
-    imagem: user5,
+    imagem: "/images/user5.jpg",
   },
   {
     nome: "Abirage Pereira",
@@ -56,7 +49,7 @@ const teamData = [
     biografia:
       "Abirage Pereira Furuma nasceu em Maputo no dia 5 de setembro de 1999. Atualmente, ele está no primeiro ano do curso de Administração Pública, onde busca adquirir conhecimentos que o ajudarão em sua trajetória profissional. Com uma vasta experiência, Abirage atuou como conselheiro leigo, trabalhou com aconselhamento psicossocial e exerceu a função de defensor comunitário, demonstrando seu compromisso em ajudar os outros e promover a justiça social. Atualmente, ele é assistente administrativo na organização Kutchindja, onde contribui para a gestão e administração da instituição. Nos seus momentos de lazer, Abirage gosta de ler livros, uma atividade que alimenta sua curiosidade e amor pelo conhecimento, além de aproveitar o tempo conversando com amigos, valorizando a importância das relações sociais.",
     frase_motivacional: "Se cair 7 vezes levante-se 8.",
-    imagem: user6,
+    imagem: "/images/user6.jpg",
   },
 ];
 
